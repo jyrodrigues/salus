bc183c06f2d8f3945dcd4de7e2917c2028dcb33f
José Rodrigues <jyrodrigues@gmail.com>
2019-10-29 11:00:23 -0300

    Add blank Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './profile.css';
+
+export function Profile() {
+    return (
+        <div className="something">
+            Hello there!
+        </div>
+    )
+}
